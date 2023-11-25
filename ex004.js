f89e9c9892028fde022667359c710ee4e7852680
Fabricio da Silva Lisboa <150758493+fablisboa2006@users.noreply.github.com>
2023-11-25 20:18:26 -0300

    dev

---
+++ ex004.js
@@ -0,0 +1,7 @@
+var recibo = 6172.10;
+        
+var nome = window.prompt('Qual é o seu nome?') 
+        document.write(`Olá ${nome}! <br>Seu nome tem ${nome.length} letras.</br>`)
+        document.write(`Seu nome em MAIÚSCULA é ${nome.toUpperCase()}</br>`)
+        document.write(`Seu nome em minúscula é ${nome.toLowerCase()}`)
+        alert("Seu salario é " + recibo);
